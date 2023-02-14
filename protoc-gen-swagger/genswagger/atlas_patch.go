380e068813ed894a20b9b81b2974e748ecec1e98
kirthana7 <kml@infoblox.com>
2023-02-14 11:18:39 +0530

    patch

--- protoc-gen-swagger/genswagger/atlas_patch.go
+++ protoc-gen-swagger/genswagger/atlas_patch.go
@@ -91,7 +91,7 @@ func atlasSwagger(b []byte, withPrivateMethods, withCustomAnnotations bool) stri
 	for pn, pi := range sw.Paths.Paths {
 		var pnElements []string
 		for _, v := range strings.Split(pn, "/") {
-			if strings.HasSuffix(v, "id.resource_id}") || strings.HasSuffix(v, ".id}") || strings.HasSuffix(v, ".value}") {
+			if strings.HasSuffix(v, "id.resource_id}") || strings.HasSuffix(v, ".id}") || strings.HasSuffix(v, "id.value}") {
 				pnElements = append(pnElements, "{id}")
 			} else {
 				pnElements = append(pnElements, v)
@@ -180,7 +180,7 @@ The service-defined string used to identify a page of resources. A null value in
 					default:
 					}
 					// Replace resource_id with id
-				} else if strings.HasSuffix(param.Name, "id.resource_id") || strings.HasSuffix(param.Name, ".id") {
+				} else if strings.HasSuffix(param.Name, "id.resource_id") || strings.HasSuffix(param.Name, ".id") || strings.HasSuffix(param.Name, "id.value}") {
 					param.Name = "id"
 					fixedParams = append(fixedParams, param)
 				} else if strings.HasPrefix(param.Description, "tagging.api.") {
